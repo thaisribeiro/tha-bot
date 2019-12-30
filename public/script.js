f5541edf9f621053abc045c33794d4be1d54e8be
Thais Ribeiro <thaisribeiro@softbox.com.br>
2019-12-30 11:29:35 -0300

    build

--- public/script.js
+++ public/script.js
@@ -22,7 +22,7 @@ const InsertTemplateInTheChat = (template) => {
 
 // Calling server and get the watson output
 const getWatsonMessageAndInsertTemplate = async (text = '') => {
-  const uri = 'http://localhost:3000/conversation/';
+  const uri = 'https://bot-tha.netlify.com/conversation/';
 
   const response = await (await fetch(uri, {
     method: 'POST',
